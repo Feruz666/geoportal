fbd1e4b5436eb4f1fdfc156785e5b460f7fa48e3
Odleral77 <odleral77@gmail.com>
2021-04-15 16:49:26 +0500

    загружал различные библеотеки

--- geoportal/config.js
+++ geoportal/config.js
@@ -1,8 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    entry: './src/index.js',
+  plugins: [new MiniCssExtractPlugin()],  
+  entry: './src/index.js',
     mode: 'development',
     output: {
         filename: 'index.js',
         path: '/Users/odiljonbokhodirov/Desktop/projects/geoportal/geoportal/static/js'
-    }
+    },
+    resolve: {
+        // ... rest of the resolve config
+        fallback: {
+          "path": require.resolve("path-browserify")
+        }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
 }
--- geoportal/src/index.js
+++ geoportal/src/index.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+// import 'materialize';
+// import 'jquery';
+// import 'mapbox-gl';
+// import '@supermap/iclient-mapboxgl';
+// import 'mapbox-gl-compare';
+import { MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
+import { Map } from "mapbox-gl";
+
+import "mapbox-gl-style-switcher/styles.css";
+
+import "mapbox-gl-style-switcher/styles.css";
+const map = new MapboxMap();
+map.addControl(new MapboxStyleSwitcherControl());
